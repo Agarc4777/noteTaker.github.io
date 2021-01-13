2243063d1b608576a600bd94df8337c94bf73068
Ignacio Garcia <69227346+Agarc4777@users.noreply.github.com>
2021-01-12 22:08:46 -0600

    added delete

--- server.js
+++ server.js
@@ -18,30 +18,49 @@ app.get("/notes", function (req, res){
     res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("*", function (req, res){
-    res.sendFile(path.join(__dirname, "/public/index.html"));
-});
-
 //Routes: API's 
 app.get("/api/notes", function (req, res){
     res.sendFile(path.join(__dirname, "/db/db.json"));
 });
 
-
 // app post. adds new note.
 app.post("/api/notes", function (req, res){
-// function to read notes from db.json
+    // function to read notes from db.json
     let newNote = req.body;
-    newNote.id = idNumber(),
-    newNote.push(newNote);
+    var notes = [];
+    fs.readFile('./db/db.json', (error, data) => {
+        if (err) return "ERROR";
+        notes = JSON.parse(data);
+        notes.push(newNote);
+        fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => 
+            err ? console.error(err) : console.log('successfully added.')
+        );
+        res.send(notes);
+    });
 });
 
 // app delete
-app.delete("/api/notes", function (req, res){
-    // function to delete notes from the db.
+app.delete("/api/notes", function (req,res){
+    var note = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+    var delNoteID = req.params.id;
+    note = note.filter(function (data){
+        return data.id != delNoteID;
+    });
+    let deletedNotes = JSON.stringify(note);
+    fs.writeFile("./db/db.json", deletedNotes, err =>{
+        if (err) throw err;
+        else {
+            return res.json(deletedNotes);
+        }
+    });    
+});
 
-
-})
-
+// moved this to end due to issues that might occur.
+app.get("*", function (req, res){
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 
 // console log for port number.
+app.listen(PORT, function() {
+    console.log("connected to port: " + PORT);
+});
